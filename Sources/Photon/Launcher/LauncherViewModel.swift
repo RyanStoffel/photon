d38733ff8305967786dc3ab6aac0cc7d5cbbfdd2
Ryan Stoffel <stoffel.thomas.ryan@gmail.com>
2026-09-14 18:45:35 +0000

    chore: allow launcher view model type length for clipboard state

--- Sources/Photon/Launcher/LauncherViewModel.swift
+++ Sources/Photon/Launcher/LauncherViewModel.swift
@@ -12,6 +12,7 @@ enum LauncherSession: Equatable {
 }
 
 @MainActor
+// swiftlint:disable:next type_body_length
 final class LauncherViewModel: ObservableObject {
   @Published var query = "" {
     didSet {
